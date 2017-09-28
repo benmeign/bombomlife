03cf39a0b0c4c0ba2c9d0bc2e59c448cf3d94786
benmeign <benedicte.meignan@gmail.com>
2017-09-28 12:02:56 +0200

    test

--- server/bin/scheduledDailyChallenge.js
+++ server/bin/scheduledDailyChallenge.js
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
 
+var User = require('../models/user.js')
+
 function sayHello() {
     console.log('Hello BomBom');
 }
 sayHello();
+
+User.find({}, (err, users) => {
+    users.forEach((user) => {
+        console.log(user.name);
+    });
+});
 
