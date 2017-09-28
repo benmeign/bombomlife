04a9fde0e025bc977c10497e7c97632b5d66880c
benmeign <benedicte.meignan@gmail.com>
2017-09-28 12:09:03 +0200

    test 2 scheduler

--- server/bin/scheduledDailyChallenge.js
+++ server/bin/scheduledDailyChallenge.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 var User = require('../models/user.js')
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/bombomlife');
 
 function sayHello() {
     console.log('Hello BomBom');
